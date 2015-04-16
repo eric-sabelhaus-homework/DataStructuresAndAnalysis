2c1fdc3921ac8db02a030814faee07cd0bcfdd79
Eric Sabelhaus <eric.sabelhaus@gmail.com>
2015-04-16 15:38:59 -0400

    this should give doge something to bark about

--- src/FinalProject_Eric_Sabelhaus/Node.java
+++ src/FinalProject_Eric_Sabelhaus/Node.java
@@ -1,24 +1,16 @@
 package FinalProject_Eric_Sabelhaus;
-
 public class Node<Edge> {
-	
 	private Edge edge;
-	
 	private Node<Edge> next;
-	
-	
 	public Edge getEdge() {
 		return edge;
 	}
-	
 	public void setEdge(Edge i) {
 		this.edge = i;
 	}
-	
 	public Node<Edge> getNext() {
 		return next;
 	}
-	
 	public void setNext(Node<Edge> next) {
 		this.next = next;
 	}
